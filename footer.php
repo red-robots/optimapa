d4b57f077554b806e79ca6bae0d08b05abbfa3c3
Austin Crane <austincrane@gmail.com>
2017-07-18 16:12:26 -0400

    add tracking

--- footer.php
+++ footer.php
@@ -17,7 +17,16 @@ Charlotte: 1927 South Tryon Street Suite 300 | Charlotte, NC 28203 | 704.338.129
 </div><!-- footer cont -->
 </div><!-- footer -->
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-102761708-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 	
 
 <?php wp_footer(); ?>
