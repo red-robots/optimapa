ebca3822920d235f3112510663db94f45a8555f2
Fritz <fre.healy@gmail.com>
2018-03-30 10:14:21 -0400

    temp commit

--- includes/category-nav-bar.php
+++ includes/category-nav-bar.php
@@ -1,11 +1,11 @@
 <?php $terms = get_terms(array('taxonomy'=>'categories', 'hide_empty'=>false));
 if(!is_wp_error($terms)&&is_array($terms)&&!empty($terms)):?>
-    <div id="subnav" class="clear-bottom">
+    <div id="subnav">
         <?php $read_more_text = get_field("read_more_text","option");
         $i = 0;
         foreach($terms as $term):?>
             <?php $fa_text = get_field("font_awesome",$term);?>
-            <div class="box js-blocks <?php if($i%4===0) echo "first";?> <?php if(($i+1)%4===0) echo "last";?>">
+            <div class="box js-blocks">
                 <a href="<?php echo get_term_link($term->term_id);?>">
                     <?php if($fa_text):?>
                         <div class="font-awesome">
